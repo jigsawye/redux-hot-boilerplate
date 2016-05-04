c9e9ce59b7af6602dc33dc53fdd85697a42ee633
jigsawye <jigsaw.ye@gmail.com>
2016-05-04 13:57:08 +0800

    Add some test to visibilityFilter

--- src/modules/__tests__/visibilityFilter.js
+++ src/modules/__tests__/visibilityFilter.js
@@ -1,9 +1,32 @@
 import test from 'ava';
-import { setVisibilityFilter } from '../visibilityFilter';
+import visibilityFilter, { setVisibilityFilter } from '../visibilityFilter';
 
 test('setVisibilityFilter should create SET_VISIBILITY_FILTER action', t => {
   t.deepEqual(setVisibilityFilter('active'), {
     type: 'SET_VISIBILITY_FILTER',
     payload: 'active',
   });
+});
+
+test('should handle initial state', t => {
+  t.deepEqual(visibilityFilter(undefined, {}), 'SHOW_ALL');
+});
+
+
+test('should handle setVisibilityFilter', t => {
+  t.deepEqual(
+    visibilityFilter('SHOW_ALL', {
+      type: 'SET_VISIBILITY_FILTER',
+      payload: 'SHOW_ACTIVE',
+    }),
+    'SHOW_ACTIVE',
+  );
+
+  t.deepEqual(
+    visibilityFilter('SHOW_ALL', {
+      type: 'SET_VISIBILITY_FILTER',
+      payload: 'SHOW_COMPLETED',
+    }),
+    'SHOW_COMPLETED',
+  );
 });
